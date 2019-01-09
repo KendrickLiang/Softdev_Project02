50214a70f3ffb42f2fba2491411aeb19d5993be6
wzhou2 <wzhou2@stuy.edu>
2019-01-09 17:51:21 -0500

    working on login

--- app.py
+++ app.py
@@ -3,10 +3,35 @@ import json, urllib, os
 from flask import Flask, render_template, flash, request, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = os.urandom(64)
 
 @app.route("/")
 def index():
-    return 'blank'
+    print(session)
+    if 'current_user' in session:
+        return render_template('home.html', message='Your farm')
+    return render_template('login.html', title="Login")
+
+@app.route("/authentication", methods=['POST'])
+def authenticate():
+    username, password = request.form['username'].strip(), request.form['password'].strip()
+    if username == '' and password == '':
+        return render_template('home.html', message='FAIL')
+    if username == 'a':#in db:
+        if password == 'a':#db.pass
+            session['current_user'] = username
+            current_user = username
+            return redirect("/")
+    return render_template('home.html', message='FAIL@')
+
+@app.route("/registration", methods=['POST'])
+def register():
+        return ''
+
+@app.route("/logout")
+def logout():
+    session.pop('current_user')
+    return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True
