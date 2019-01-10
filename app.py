5dc04ec793959a40ca99f6f2aa376884b74f7cd0
wzhou2 <wzhou2@stuy.edu>
2019-01-10 14:06:35 -0500

    perfecting user login/register

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import json, urllib, os
 
 from flask import Flask, render_template, flash, request, session, redirect, url_for
 
+from util import db as silo
+
+global DB_FIlE = "/data/silo.db"
 app = Flask(__name__)
 app.secret_key = os.urandom(64)
 
@@ -14,19 +17,31 @@ def index():
 
 @app.route("/authentication", methods=['POST'])
 def authenticate():
+    '''
+    Checks if the user, password pair sent is valid
+    If not sent them back to login page
+    If valid - log them in and send them to the home page
+    '''
     username, password = request.form['username'].strip(), request.form['password'].strip()
     if username == '' and password == '':
-        return render_template('home.html', message='FAIL')
-    if username == 'a':#in db:
-        if password == 'a':#db.pass
-            session['current_user'] = username
-            current_user = username
-            return redirect("/")
+        return redirect("/")
+    if silo.checkUser(username, password):
+        session['current_user'] = username
+        current_user = username
+        return redirect("/")
     return render_template('home.html', message='FAIL@')
 
 @app.route("/registration", methods=['POST'])
 def register():
-        return ''
+    '''
+    Checks if the username sent is in database
+    if Yes - redirect back to register page
+    if No - add user, password into database and send them to the home page
+    '''
+    if silo.addUser(request.form['username'].strip(), request.form['password'].strip()):
+        session['current_user'] = username
+        return render_template('home.html', message='WELCOME'+request.form['username'].strip())
+    render_template('login.html')
 
 @app.route("/logout")
 def logout():
--- util/db.py
+++ util/db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-DB_FIlE(../data/silo.db)
+DB_FIlE = "../data/silo.db"
 
 def createTables():
     db = sqlite3.connect(DB_FILE)
@@ -11,3 +11,33 @@ def createTables():
     c.execute("CREATE TABLE IF NOT EXISTS trades(user TEXT, item TEXT, count INT, cost REAL)")
     db.commit()
     db.close()
+
+def addUser(user, password):
+    user_list = getUsers()
+    if user in user_list:
+        return False
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("INSERT INTO users values (?, ?, ?, ?)", (user, password, 0, ''))
+    db.commit()
+    db.close()
+    return True
+
+def checkUser(user, password):
+    user_list = getUsers()
+    if user in user_list:
+        if user_list[user] == password:
+            return True
+
+    return False
+
+def getUsers():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    # Gets the list of users in database
+    c.execute("SELECT usernames FROM users")
+    user_list = c.fetchall()
+
+    db.commit()
+    db.close()
+    return user_list
