a0b9cdaaf1fe87d9dc2e6e86a130a2de941932a7
wzhou2 <wzhou2@stuy.edu>
2019-01-11 15:00:44 -0500

    remove console.log

--- static/farm.js
+++ static/farm.js
@@ -1,9 +1,9 @@
 console.log("connected");
 
 var farm = document.getElementsByClassName('farm-tile');
-console.log(farm);
+//console.log(farm);
 for(x = 0; x<farm.length; x++) {
-    console.log(x,farm[x]);
+    //console.log(x,farm[x]);
     farm[x].addEventListener("mouseover", function(e){
         console.log(e);
     })
