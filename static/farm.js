59138804f5f02e9a7b0e9746cb1e176e00946528
wzhou2 <wzhou2@stuy.edu>
2019-01-12 03:22:29 -0500

    working on frontEnd to simulate planting crops

--- static/farm.js
+++ static/farm.js
@@ -5,12 +5,30 @@ var farm = document.getElementsByClassName('farm-tile');
 for(x = 0; x<farm.length; x++) {
     //console.log(x,farm[x]);
     farm[x].addEventListener("mouseover", function(e){
-        console.log(e);
+        //console.log(e);
     })
-    console.log(farm[x].innerHTML == 'Dirt');
+    //console.log(farm[x].innerHTML == 'Dirt');
     if (farm[x].innerHTML == 'Dirt') {
         farm[x].addEventListener("click", function(e) {
             this.innerHTML = "CLICK";
+            cropSelect(this)
         })
     }
+}
+
+var cropSelect = function(tile) {
+    console.log(tile);
+    console.log(document.getElementById('cropType').innerHTML);
+    var selector = document.getElementById('modal-button');
+    document.addEventListener("click", function(e){
+        console.log('Here');
+        console.log(document.getElementById('cropType').value);
+        tile.innerHTML = document.getElementById('cropType').value;
+        tile.removeEventListener("click", function(e) {
+            this.innerHTML = "CLICK";
+            cropSelect(this)
+        });
+        tile.addEventListener("mouseover", function(e){
+        });
+    })
 }
